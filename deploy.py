53825ae23c4207d8db538de80ec37eb4f4914fc0
scott <scott@tams.tech>
2018-03-19 00:26:06 -0400

    switched to mustache template.

--- deploy.py
+++ deploy.py
@@ -7,6 +7,21 @@ final_dockerfile = before_substitutions.substitute(
     {
         "application": PACKAGE_NAME,
         "package": PACKAGE_NAME
+from pystache import render
+working_directory = dirname(realpath(__file__))
+PACKAGE_NAME = "geany"
+with open(getpath(working_directory, "Dockerfile.pytemplate"), 'r') as dockerfile:
+    before_substitutions = dockerfile.read()
+final_dockerfile = render(
+    template=before_substitutions,
+    context={
+        'package': PACKAGE_NAME,
+        'application': PACKAGE_NAME,
+        'distro': {
+            'name': "ubuntu",
+            'version': 16.04,
+            'pkg_mngr_cmd': 'apt-get update && apt-get install -y'
+        }
     }
 )
 dockerfile = open("Dockerfile", 'w')
