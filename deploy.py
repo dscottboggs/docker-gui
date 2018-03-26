56fbcead50db58a1f67ee91e97034b8c991a52ad
scott <scott@tams.tech>
2018-03-26 09:41:38 -0400

    Migrated to class/object oriented style rather than procedural.

--- deploy.py
+++ deploy.py
@@ -1,31 +1,82 @@
+from distros import getdistro
+test_input = {
+    "package": "x11-apps",
+    "application": "xeyes",
+    "distro": getdistro("ubuntu", "16.04"),
+}
+
 from yaml import dump
-from os.path import dirname, realpath
+from os.path import dirname, realpath, isdir
 from os.path import join as getpath
 from os import environ
+from os import makedirs as mkdir
 from pystache import render
-working_directory = dirname(realpath(__file__))
-PACKAGE_NAME = "simon"
-with open(getpath(working_directory, "Dockerfile.pytemplate"), 'r') as dockerfile:
-    before_substitutions = dockerfile.read()
-final_dockerfile = render(
-    template=before_substitutions,
-    context={
-        'package': PACKAGE_NAME,
-        'application': PACKAGE_NAME,
-        'distro': {
-            'name': "ubuntu",
-            'version': 16.04,
-            'pkg_mngr_cmd': {
-                'update': 'apt-get update',
-                'install': 'apt-get install -y'
+
+class Application():
+    """An application to deploy"""
+    def __init__(self, package: str, application: str, distro: str, version: str=''):
+        self.package = package
+        self.application = application
+        self.distro = getdistro(distro, version)
+
+    def init_files(self):
+        self.working_directory = getpath('usr', 'share', 'docker-gui')
+        if not isdir(working_directory):
+            if access(working_directory, F_OK):
+                raise RuntimeError("Directory to work in exists as a file.")
+            else:
+                mkdir(working_directory, mode=0o755)
+        with open(getpath(working_directory, "Dockerfile.pytemplate"), 'r') as dockerfile:
+            self.before_substitutions = dockerfile.read()
+        self.application_directory = getpath(
+            self.working_directory,
+            self.application
+        )
+
+    def write_desktop_file(self):
+        with open(
+                    getpath(
+                        'usr',
+                        'share',
+                        'applications',
+                        "%s.docker.desktop" % test_input['application']
+                    )
+                ) as desktop_file:
+            desktop_file.write(render(
+                text=dedent("""
+                    [Desktop Entry]
+                    Version=From {{ distro }}
+                    Name={{ application }}
+                    Exec={{ command }}
+                    Terminal=false
+                    Type=Application
+                    Categories=Containerized
+                    """
+                ),
+                context={
+                    'commmand': f"docker run {self.image_name}"
+                    'application': self.application,
+                    'distro': self.distro.distro
+                }
+            ))
+
+    def render_dockerfile(self):
+        self.final_dockerfile = render(
+            template=self.before_substitutions,
+            context={
+                'package': self.package,
+                'application': self.application,
+                'distro': self.distro,
+                'uid': environ['UID'],
+                'gid': environ['GID']
             }
-        },
-        'uid': 1000,
-        'gid': 1000
-    }
-)
-with open(getpath(working_directory, "Dockerfile"), 'w') as dockerfile:
-    dockerfile.write(final_dockerfile)
+        )
+        with open(getpath(
+                    self.application_directory, "Dockerfile"
+                ),'w') as dockerfile:
+            dockerfile.write(final_dockerfile)
+
+
 
 composition = {
     'version': '2.0',
@@ -45,5 +96,12 @@ composition = {
     },
     'networks': {'application': None}
 }
-with open(getpath(working_directory, "docker-compose.yml"), 'w') as compose_file:
+compose_file_path = getpath(
+    application_directory,
+    "docker-compose.yml"
+)
+with open(
+            compose_file_path,
+            'w'
+        ) as compose_file:
     compose_file.write(dump(composition))
