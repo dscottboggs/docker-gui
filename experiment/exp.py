818e4942002da2be55a1f2acc8b3950d56ba23fe
scott <scott@tams.tech>
2018-04-11 17:50:46 -0400

    removed experiment folder

--- experiment/exp.py
+++
@@ -1,5 +0,0 @@
-"""fuck your docstring, it's just an experiment."""
-import docker
-dc = docker.DockerClient('unix://var/run/docker.sock', version='1.30')
-thispath = "/home/scott/Documents/code/docker-gui/experiment"
-dc.images.build(path=thispath, tag='experiment')
